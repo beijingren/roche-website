f46cf3650af5b3d75e616523983afc28cabfc9ec
David Höppner <0xffea@gmail.com>
2013-11-14 17:39:32 +0100

    Add browser application

--- roche/urls.py
+++ roche/urls.py
@@ -7,6 +7,12 @@ from django.conf.urls import url
 from django.conf.urls.i18n import i18n_patterns
 from django.views.generic import TemplateView
 
+
+browser_patters = patterns('',
+    url(r'^$', TemplateView.as_view(template_name='browser/index.html')),
+)
+
 urlpatterns = i18n_patterns('',
     url(r'^$', TemplateView.as_view(template_name='roche/index.html')),
+    url(r'^browse/$', include(browser_patters, namespace='browse')),
 )
