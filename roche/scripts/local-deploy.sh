129f7b8dcf76ef9be2244a0285a04587cad8b24f
David Höppner <0xffea@gmail.com>
2013-11-17 18:26:42 +0100

    Improve local deploy script

--- roche/scripts/local-deploy.sh
+++ roche/scripts/local-deploy.sh
@@ -1,6 +1,30 @@
 
+# Keep in sync with dedalus-infrastructure (but without apache2)
+sudo apt-get install -y		\
+	python-django		\
+	python-django-mptt	\
+	python-django-sekizai	\
+	python-django-south	\
+	python-imaging		\
+	python-lxml		\
+	python-markdown		\
+	python-pip		\
+	python-ply		\
+	python-psycopg2		\
+	python-sorl-thumbnail	\
+	gettext			\
+	git			\
+	libpython-dev
+
+sudo pip install -r requirements.txt
+
+python manage.py compilemessages
+
+# Docker link variables
 DOCKER_PASSWORD=''
 DB_PORT_5432_TCP_ADDR='localhost'
 DB_PORT_5432_TCP_PORT='5432'
 
-export DOCKER_PASSWORD DB_PORT_5432_TCP_ADDR DB_PORT_5432_TCP_PORT
+export DOCKER_PASSWORD
+export DB_PORT_5432_TCP_ADDR
+export DB_PORT_5432_TCP_PORT
