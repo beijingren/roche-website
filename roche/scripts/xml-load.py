f48eb543c3ae2222a71080592ae8932c227dc605
David Höppner <0xffea@gmail.com>
2013-11-29 20:41:54 +0100

    Fix relative path in relation to app root dir

--- roche/scripts/xml-load.py
+++ roche/scripts/xml-load.py
@@ -1,7 +1,11 @@
 # coding=utf-8
 
+#
+# Must be called in roche root dir
+#
+
 import sys
-sys.path.append('../../')
+sys.path.append('.')
 import roche.settings
 
 from eulexistdb.db import ExistDB
@@ -15,6 +19,6 @@ xmldb = ExistDB(timeout=30)
 xmldb.createCollection('docker', True)
 xmldb.createCollection(u'docker/浙江大學圖書館', True)
 
-with open('../../../dublin-store/db/test_001.xml') as f:
+with open('../dublin-store/db/test_001.xml') as f:
     xmldb.load(f, '/docker/001.xml', True)
 
