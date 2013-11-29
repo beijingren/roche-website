02adcdfc4c32d88d88074924b16a1b6daf9e4cf2
David Höppner <0xffea@gmail.com>
2013-11-29 20:20:18 +0100

    Add script to load xml documents into exist

---
+++ roche/scripts/xml-load.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+
+import sys
+sys.path.append('../../')
+import roche.settings
+
+from eulexistdb.db import ExistDB
+from roche.settings import EXISTDB_SERVER_URL
+
+#
+# Timeout higher?
+#
+xmldb = ExistDB(timeout=30)
+
+xmldb.createCollection('docker', True)
+xmldb.createCollection(u'docker/浙江大學圖書館', True)
+
+with open('../../../dublin-store/db/test_001.xml') as f:
+    xmldb.load(f, '/docker/001.xml', True)
+
