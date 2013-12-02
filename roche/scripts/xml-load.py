8df3076b6315a74e57ee27fe3478d36737be0ff9
David Höppner <0xffea@gmail.com>
2013-12-02 18:31:13 +0100

    Load other resources into exist-db

--- roche/scripts/xml-load.py
+++ roche/scripts/xml-load.py
@@ -30,3 +30,13 @@ for (dirpath, dirnames, filenames) in walk('浙江大學圖書館'):
         for filename in filenames:
             with open(dirpath + '/' + filename) as f:
                 xmldb.load(f, 'docker/texts' + '/' + dirpath + '/' + filename, True)
+
+#
+# Load resources
+#
+for (dirpath, dirnames, filenames) in walk('resources'):
+    xmldb.createCollection('docker' + '/' + dirpath, True)
+    if filenames:
+        for filename in filenames:
+            with open(dirpath + '/' + filename) as f:
+                xmldb.load(f, 'docker' + '/' + dirpath + '/' + filename, True)
