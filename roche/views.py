268e3812c063cfea50426d3927c8ceaa8c395b32
David Höppner <0xffea@gmail.com>
2014-05-19 13:10:43 +0200

    Fetch number of author and texts

--- roche/views.py
+++ roche/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.template import RequestContext
 
+from eulexistdb.db import ExistDB
+from eulexistdb.query import QuerySet
+
+from browser.models import RocheTEI
+
 
 def index(request):
     # XML and SPARQL numbers
     # Page should be cached
 
-    data = {'number_texts': 4, 'number_authors': 4}
+    # Count texts and authors
+    qs = QuerySet(using=ExistDB(), xpath='/tei:TEI', collection='docker/texts/', model=RocheTEI)
+    qs = qs.only('author').filter(chapter='1')
+    number_texts = qs.count()
+    number_authors = qs.distinct().count()
+
+    data = {'number_texts': number_texts, 'number_authors': number_authors}
     return render(request, 'roche/index.html', data)
