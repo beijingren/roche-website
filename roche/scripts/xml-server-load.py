7d3c85f3d8c600b349abf428f6181cd05cb983a0
David Höppner <0xffea@gmail.com>
2014-08-21 11:06:44 +0200

    Script to upload xml files to the server

---
+++ roche/scripts/xml-server-load.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+
+#
+# Must be called in roche root dir
+#
+
+import os
+
+
+from os import walk
+from eulexistdb.db import ExistDB
+
+#
+# Timeout higher?
+#
+xmldb = ExistDB('http://54.220.97.75:8080/exist')
+
+xmldb.createCollection('docker', True)
+xmldb.createCollection('docker/texts', True)
+
+os.chdir('../dublin-store')
+
+for (dirpath, dirnames, filenames) in walk('浙江大學圖書館'):
+    xmldb.createCollection('docker/texts' + '/' + dirpath, True)
+    if filenames:
+        for filename in filenames:
+            with open(dirpath + '/' + filename) as f:
+                print "--" + dirpath + '/' + filename
+                xmldb.load(f, 'docker/texts' + '/' + dirpath + '/' + filename, True)
+
+#
+# Load resources
+#
+for (dirpath, dirnames, filenames) in walk('resources'):
+    xmldb.createCollection('docker' + '/' + dirpath, True)
+    if filenames:
+        for filename in filenames:
+            with open(dirpath + '/' + filename) as f:
+                xmldb.load(f, 'docker' + '/' + dirpath + '/' + filename, True)
